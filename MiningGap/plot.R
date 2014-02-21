abaddb1d45b4c6e8b99838637fc9b1d8454b5752
John Mount <jmount@win-vector.com>
2014-02-20 17:59:42 -0800

    fix estimates

--- MiningGap/plot.R
+++ MiningGap/plot.R
@@ -1,11 +1,17 @@
 
 # load libraries
+require(RCurl)
 require(ggplot2)
 require(mgcv)
 require(reshape2)
 
 # Load data, and make sure x is increasing
-d <- read.table('NMsg.csv',header=T,sep=',')
+urlBase <- 
+   'https://raw2.github.com/WinVector/Examples/master/MiningGap/'
+mkCon <- function(nm) {
+   textConnection(getURL(paste(urlBase,nm,sep='')))
+}
+d <- read.table(mkCon('NMsg.csv'),header=T,sep=',')
 d <- d[order(d$DaysBeforeAfterRelationship),]
 
 # plot something similar to original graph
@@ -14,27 +20,30 @@ ggplot(data=d,aes(x=DaysBeforeAfterRelationship,
    geom_point() + 
    stat_smooth(method = "gam", formula = y ~ s(x))
 
-# What would individuals generating posts
-# as a Poisson process with a given intensity
-# look like around 100 days before forming
-# a relationship, right before forming a relationship,
-# and long after a relationship is formed?
 earlyIntensity <- mean(
    d[d$DaysBeforeAfterRelationship >= -100 &
    d$DaysBeforeAfterRelationship < -80,
    'NumberOfTimelinePosts'])
 print(earlyIntensity)
+## [1] 1.622637
 nearIntensity <- mean(
    d[d$DaysBeforeAfterRelationship >= -20 &
      d$DaysBeforeAfterRelationship < 0,
    'NumberOfTimelinePosts'])
 print(nearIntensity)
+## [1] 1.653082
 afterIntensity <- mean(
    d[d$DaysBeforeAfterRelationship >= 80 &
      d$DaysBeforeAfterRelationship < 100,
    'NumberOfTimelinePosts'])
 print(afterIntensity)
-set.seed(2315531)
+## [1] 1.539452
+
+# What would individuals generating posts
+# as a Poisson process with a given intensity
+# look like around 100 days before forming
+# a relationship, right before forming a relationship,
+# and long after a relationship is formed?
 density <- data.frame(NumberOfTimelinePosts=0:10)
 density$nearCounts <- dpois(
    density$NumberOfTimelinePosts,
@@ -58,7 +67,7 @@ print(dm[dm$NumberOfTimelinePosts==4,])
 # If the original data was generated from
 # independent individuals with slowly varying
 # Poisson intensity, then we could estimate the
-# degree of aggregation to see the mean^2/variance
+# degree of aggregation to see the mean/variance
 # ration in the plot.
 odd <- 2*(1:(dim(d)[[1]]/2))-1
 varEst <- sum((d[odd,'NumberOfTimelinePosts']
@@ -71,3 +80,8 @@ print(meanEst)
 kEst <- meanEst/varEst
 print(kEst)
 ## [1] 69851.47
+print(dim(d)[[1]])
+## [1] 193
+print(kEst*dim(d)[[1]])
+## [1] 13481334
+
