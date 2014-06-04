7be576ac1188f8faeaba61922e757753e02127d7
John Mount <jmount@win-vector.com>
2014-06-04 15:52:08 -0700

    better plot

--- modelStripping/timeLM.R
+++ modelStripping/timeLM.R
@@ -29,12 +29,14 @@ for(n in seq(1000,10000,1000)) {
    ti <- data.frame(n=n,stepTime=timeStep(n))
    plotFrameStep <- rbind(plotFrameStep,ti)
 }
-uB <- max(aggregate(stepTime~n,data=plotFrameStep,
+uBr <- max(aggregate(stepTime~n,data=plotFrameStep,
    FUN=function(x) { quantile(x,0.9) })$stepTime)
-lB <- min(aggregate(stepTime~n,data=plotFrameStep,
-   FUN=function(x) { quantile(x,0.9) })$stepTime)
+lBr <- min(aggregate(stepTime~n,data=plotFrameStep,
+   FUN=function(x) { quantile(x,0.1) })$stepTime)
+uB <- uBr + (uBr-lBr)/10
+lB <- max(0,lBr - (uBr-lBr)/10)
 ggplot(data=plotFrameStep,aes(x=n,y=stepTime)) + 
    geom_boxplot(aes(group=n),outlier.size=0) + 
    coord_cartesian(ylim=c(lB,uB)) +
    geom_smooth()
-
+ggsave(filename='lmstepTimes.png')
