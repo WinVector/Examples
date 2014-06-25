d2beb4d21d3fdc6a1ba49e9639caf9194047da4b
John Mount <jmount@win-vector.com>
2014-06-25 12:38:42 -0700

    add frequentist/Bayes exmaple

---
+++ freq/R/freq.md
@@ -0,0 +1,111 @@
+
+```
+## [1] ""
+## [1] "***** nSides = 6 kFlips = 1"
+## [1] "full rank"
+## [1] TRUE
+## [1] "bias free determined solution"
+## [1] -4.491e-16  1.000e+00
+## [1] "standard empirical solution"
+## [1] 0 1
+## [1] "losses for standard empirical solution"
+## [1] 0.1389 0.2222 0.2500 0.2222 0.1389
+## [1] "Bayes solution"
+## [1] 0.3889 0.6111
+## [1] "losses for Bayes solution"
+## [1] 0.07407 0.02778 0.01235 0.02778 0.07407
+## [1] "Bayes max loss improvement"
+## [1] 0.1759
+## [1] "Bayes solution bias check (failed)"
+## [1]  0.2593  0.1296  0.0000 -0.1296 -0.2593
+## [1] ""
+## [1] ""
+## [1] "***** nSides = 6 kFlips = 2"
+## [1] "full rank"
+## [1] TRUE
+## [1] "bias free determined solution"
+## [1] 0.0 0.5 1.0
+## [1] "standard empirical solution"
+## [1] 0.0 0.5 1.0
+## [1] "losses for standard empirical solution"
+## [1] 0.06944 0.11111 0.12500 0.11111 0.06944
+## [1] "Bayes solution"
+## [1] 0.3182 0.5000 0.6818
+## [1] "losses for Bayes solution"
+## [1] 0.05418 0.02594 0.01653 0.02594 0.05418
+## [1] "Bayes max loss improvement"
+## [1] 0.07082
+## [1] "Bayes solution bias check (failed)"
+## [1]  0.2121  0.1061  0.0000 -0.1061 -0.2121
+## [1] ""
+## [1] ""
+## [1] "***** nSides = 6 kFlips = 3"
+## [1] "full rank"
+## [1] TRUE
+## [1] "bias free determined solution"
+## [1] -2.930e-16  3.333e-01  6.667e-01  1.000e+00
+## [1] "standard empirical solution"
+## [1] 0.0000 0.3333 0.6667 1.0000
+## [1] "losses for standard empirical solution"
+## [1] 0.04630 0.07407 0.08333 0.07407 0.04630
+## [1] "Bayes solution"
+## [1] 0.2748 0.4111 0.5889 0.7252
+## [1] "losses for Bayes solution"
+## [1] 0.04134 0.02390 0.01860 0.02390 0.04134
+## [1] "Bayes max loss improvement"
+## [1] 0.04199
+## [1] "Bayes solution bias check (failed)"
+## [1]  0.17937  0.08853  0.00000 -0.08853 -0.17937
+## [1] ""
+## [1] ""
+## [1] "*****"
+## [1] ""
+## [1] "***** nSides = 6 kFlips = 7"
+## [1] 7
+## [1] "is full rank"
+## [1] FALSE
+## [1] "can extend to full rank"
+## [1] TRUE
+## [1] "confirm null vecs"
+## [1] TRUE
+## [1] "empirical solution"
+## [1] 0.0000 0.1429 0.2857 0.4286 0.5714 0.7143 0.8571 1.0000
+## [1] "empirical solution losses"
+## [1] 0.01984 0.03175 0.03571 0.03175 0.01984
+## [1] "new solution"
+## [1] 0.03191 0.11184 0.29667 0.43917 0.56083 0.70333 0.88816 0.96809
+## [1] "new solution losses"
+## [1] 0.01904 0.03269 0.03549 0.03269 0.01904
+## [1] "new solution bias checks"
+## [1] 2.776e-17 1.110e-16 0.000e+00 0.000e+00 0.000e+00
+## [1] "new solution max loss improvement"
+## [1] 0.000222
+## [1] "new solution individual loss changes"
+## [1]  0.0007983 -0.0009417  0.0002220 -0.0009417  0.0007983
+## [1] "bayes solution"
+## [1] 0.2031 0.2514 0.3360 0.4439 0.5561 0.6640 0.7486 0.7969
+## [1] "bayes losses"
+## [1] 0.01878 0.01726 0.01868 0.01726 0.01878
+## [1] "sum bayes losses"
+## [1] 0.09076
+## [1] "max bayes losses"
+## [1] 0.01878
+## [1] ""
+## [1] "polished max soln"
+## [1] 0.1900 0.2275 0.3455 0.4577 0.5423 0.6545 0.7725 0.8100
+## [1] "polished max losses"
+## [1] 0.01841 0.01839 0.01843 0.01839 0.01841
+## [1] "polished max losses max"
+## [1] 0.01843
+## [1] "polished max improvement"
+## [1] 0.0003476
+## [1] ""
+## [1] "polished sum soln"
+## [1] 0.2031 0.2514 0.3360 0.4439 0.5561 0.6640 0.7486 0.7969
+## [1] "polished sum losses"
+## [1] 0.01878 0.01726 0.01868 0.01726 0.01878
+## [1] "polished sum losses sum"
+## [1] 0.09076
+## [1] "polished sum improvement"
+## [1] 0
+```
---
+++ freq/python/freq.py
@@ -0,0 +1,210 @@
+
+from scipy.special import binom
+from math import ceil
+from numpy import matrix,zeros
+import numpy
+import scipy
+import scipy.optimize
+from rank_nullspace import rank,nullspace
+
+# python freq.py > freq.txt
+
+# In all cases we are model the observed number of wins (winsSeen)
+# found in flipping a coin kFlips times where the probability of
+# winning on each flip is given by the probility pWin 
+# (pWin in the set {1/nSides, ... (nSides-1)/nSides}).
+
+def drangei(start, stop, step):
+  d = []
+  r = start
+  while r <= stop:
+    d.append(r)
+    r += step
+  return d
+
+
+# Write down the linear conditions that confirm a vector
+# of estimates of length kFlips+1 where the entry winsSeen+1
+# represents the estimated expected value of pWin give
+# we observed winsSeen successes in kFlips trials.
+# We are writing one check condition for each possible value
+# of the unknown win probably pWin in the set {1/nSides, ... (nSides-1)/nSides}.
+def freqSystem(nSides,kFlips,stepMult=1):
+  pSeq = [ j/float(nSides) for j in range(1,nSides)]
+  if(stepMult>1):
+     pSeq = drangei(1/float(nSides),(nSides-1)/float(nSides),1/float(nSides*stepMult))
+  a = matrix([[binom(kFlips,winsSeen) * pWin**winsSeen * (1-pWin)**(kFlips-winsSeen) for winsSeen in range(kFlips+1)] for pWin in pSeq])
+  b = matrix([[pWin] for pWin in pSeq])
+  return {'a':a,'b':b}
+
+
+# Build the tradtinal frequentist emprical estimates of
+# the expected value of the unknown quantity pWin
+# for each possible observed outcome of number of wins
+# seen in kFlips trials
+def empiricalMeansEstimates(nSides,kFlips):
+  return numpy.array([ j/float(kFlips) for j in range(kFlips+1) ])
+
+
+
+# Build the Bayes estimate of expected values from uniform priors
+# on the unknown probility pWin (in the set {1/nSides, ... (nSides-1)/nSides})
+# seen in kFlips trials
+def bayesMeansEstimates(nSides,kFlips):
+  e = zeros(kFlips+1)
+  for winsSeen in range(kFlips+1):
+    posteriorProbs = zeros(nSides-1)
+    for i in range(1,nSides):
+      pWin = i/float(nSides)
+      posteriorProbs[i-1] = binom(kFlips,winsSeen) * pWin**winsSeen * (1-pWin)**(kFlips-winsSeen)
+    posteriorProbs = posteriorProbs/sum(posteriorProbs)
+    e[winsSeen] = sum(posteriorProbs*range(1,nSides))/float(nSides)
+  return numpy.array(e)
+
+
+# Compute for a given assumed win probabilty pWin
+# the expected loss (under outcomes distributed 
+# as len(ests)-1 flips with probility Win)
+# of the estimates ests.
+def lossFn(pWin,ests):
+  kFlips = len(ests)-1
+  loss = 0.0
+  for winsSeen in range(kFlips+1):
+    probObservation = binom(kFlips,winsSeen) * pWin**winsSeen * (1-pWin)**(kFlips-winsSeen)
+    loss = loss + probObservation*(ests[winsSeen]-pWin)**2
+  return loss
+
+
+
+# Compute for all win probabilities
+# pWin in the set {1/nSides, ... (nSides-1)/nSides}
+# the expected loss (under outcomes distributed 
+# as len(ests)-1 flips with probility Win)
+# of the estimates ests.
+def losses(nSides,ests):
+  return numpy.array([ lossFn(j/float(nSides),ests) for j in range(1,nSides) ])
+
+
+def flatten(x):
+   return numpy.asarray(x).reshape(-1) 
+
+def matMulFlatten(a,x): 
+   return flatten(a * numpy.matrix(numpy.reshape(x,[a.shape[1],1])))
+
+nSides = 6
+for kFlips in range(1,4):
+  print
+  print '***** nSides =',nSides,'kFlips =',kFlips
+  # first check insisting on unbiasedness
+  # completely determines the estimate for 
+  # one flip from a nSides-slides system
+  sNK = freqSystem(nSides,kFlips)
+  # print sNK
+  print 'full rank'
+  print rank(sNK['a'].T * sNK['a'])==kFlips+1
+  print 'bias free determined solution'
+  print flatten(numpy.linalg.solve(sNK['a'].T * sNK['a'],sNK['a'].T * sNK['b']))
+  print 'standard empirical solution'
+  print empiricalMeansEstimates(nSides,kFlips)
+  print 'losses for standard empirical solution'
+  print losses(nSides,empiricalMeansEstimates(nSides,kFlips))
+  
+  # now show the bayes solution has smaller loss
+  bayesSoln = bayesMeansEstimates(nSides,kFlips)
+  print 'Bayes solution'
+  print bayesSoln
+  print 'losses for Bayes solution'
+  print losses(nSides,bayesSoln)
+  print 'Bayes max loss improvement'
+  print max(losses(nSides,empiricalMeansEstimates(nSides,kFlips))) - max(losses(nSides,bayesSoln))
+  print 'Bayes solution bias check (failed)'
+  print matMulFlatten(sNK['a'],bayesSoln) - flatten(sNK['b'])
+  print
+
+
+print
+print '*****'
+# now show a underdetermined system allows more solutions
+kFlips = 7
+# confirm more probs would completely determine this situation (should by analogy to the moment curve)
+print '***** nSides =',nSides,'kFlips =',kFlips
+sU = freqSystem(nSides,kFlips)
+print 'is full rank'
+print rank(sU['a'].T * sU['a'])==kFlips+1
+print 'can extend to full rank'
+sCheck = freqSystem(nSides,kFlips,stepMult=2)
+print rank(sCheck['a'].T * sCheck['a'])==kFlips+1
+wiggleRoom = nullspace(sU['a'])
+print 'confirm null vecs'
+wiggleDim = wiggleRoom.shape[1]
+print (wiggleRoom.shape[0]==kFlips+1) & (wiggleDim + nSides-1==kFlips+1) & (numpy.matrix.max(abs(sU['a'] * wiggleRoom))<1.0e-12)
+baseSoln = empiricalMeansEstimates(nSides,kFlips)
+print 'empirical solution'
+print baseSoln
+baseLosses = losses(nSides,baseSoln)
+print 'empirical solution losses'
+print baseLosses
+
+def wsoln(x):
+   return baseSoln + matMulFlatten(wiggleRoom,x)
+
+def maxloss(x):
+   return max(losses(nSides,wsoln(x)))-max(baseLosses)
+
+opt = scipy.optimize.minimize(maxloss,zeros(wiggleDim),method='Powell')
+newSoln = wsoln(opt['x'])
+print 'new solution'
+print newSoln
+print 'new solution losses'
+print losses(nSides,newSoln)
+print 'new solution bias checks'
+print matMulFlatten(sU['a'],newSoln) - flatten(sU['b'])
+print 'new solution max loss improvement'
+print max(baseLosses)-max(losses(nSides,newSoln))
+print 'new solution individual loss changes'
+print baseLosses-losses(nSides,newSoln)
+bayesSoln = bayesMeansEstimates(nSides,kFlips)
+bayesLosses = losses(nSides,bayesSoln)
+print 'bayes solution'
+print bayesSoln
+print 'bayes losses'
+print bayesLosses
+print 'sum bayes losses'
+print sum(bayesLosses)
+print 'max bayes losses'
+print max(bayesLosses)
+print
+
+# see if we can improve on Bayes by max criterion
+def wsolnF(x):
+  return bayesSoln + x
+
+def maxlossF(x):
+  return max(losses(nSides,wsolnF(x)))-max(bayesLosses)
+
+optM = scipy.optimize.minimize(maxlossF,zeros(len(bayesSoln)),method='Powell')
+maxPolished = wsolnF(optM['x'])
+print 'polished max soln'
+print maxPolished
+print 'polished max losses'
+print losses(nSides,maxPolished)
+print 'polished max losses max'
+print max(losses(nSides,maxPolished))
+print 'polished max improvement'
+print max(bayesLosses)-max(losses(nSides,maxPolished))
+
+# see if we can improve on Bayes by max criterion
+def sumlossF(x):
+   return sum(losses(nSides,wsolnF(x)))-sum(bayesLosses)
+
+optS = scipy.optimize.minimize(sumlossF,zeros(len(bayesSoln)),method='Powell')
+polishedSum = wsolnF(optS['x'])
+print 'polished sum soln'
+print polishedSum
+print 'polished sum losses'
+print losses(nSides,polishedSum)
+print 'polished sum losses sum'
+print sum(losses(nSides,polishedSum))
+print 'polished sum improvement'
+print sum(bayesLosses)-sum(losses(nSides,polishedSum))
+
