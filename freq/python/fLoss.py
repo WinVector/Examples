5dd788e6d4adfdfcf8dd3aa4522f63a4d34df1db
John Mount <jmount@win-vector.com>
2014-07-17 16:22:53 -0700

    work on analytic solution

--- freq/python/fLoss.py
+++ freq/python/fLoss.py
@@ -11,10 +11,12 @@ def ncr(n, r):
     denom = reduce(op.mul, xrange(1, r+1))
     return numer//denom
 
-
+def realsoln(si):
+   return all([ abs(complex(sij).imag)<1.e-6 for sij in si.values() ])
 
 p = symbols('p')
-for k in [ k+1 for k in range(3) ]:
+
+for k in [ k+1 for k in range(4) ]:
    print '*******************'
    print k
    phis = [ symbols(str('phi_'+str(h) + '_' + str(k))) for h in range(k+1) ]
@@ -22,14 +24,16 @@ for k in [ k+1 for k in range(3) ]:
    polyTerms = collect(expand(poly),p,evaluate=False)
    eqns = [ polyTerms[p**(pow+1)] for pow in range(len(polyTerms)-1) ]
    soln1 = solve(eqns)
-   print soln1
    dcterm = polyTerms[p**0]
-   costs = [ float(expand(dcterm.subs(si))) for si in soln1 ]
-   soln = soln1[numpy.argmin(costs)]
+   costs = { i:float(expand(dcterm.subs(soln1[i]))) for i in range(len(soln1)) if realsoln(soln1[i]) }
+   minCost = min(costs.values())
+   index = [ i for i in costs.keys() if costs[i] <= minCost ][0]
+   soln = soln1[index]
    print soln
    print float(expand(dcterm.subs(soln)))
    numsoln = { k:float(soln[k]) for k in soln.keys() }
    print numsoln
    print '*******************'
+
 
 
