b5b88468177d0bf74c6817fddcdd48a45b196df2
John Mount <jmount@win-vector.com>
2014-09-09 10:31:22 -0700

    remove old log add bash script to run

---
+++ Count/runC.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CDIR=lib
+
+mkdir bin
+
+CP=$CDIR/Colt-1.2.0.jar:$CDIR/jblas-1.2.3.jar:$CDIR/commons-math3-3.0.jar:$CDIR/WVLPSolver.jar
+javac -cp $CP -sourcepath src -d bin `find src -name \*.java`
+
+java -Xmx5G -cp $CP:bin com.mzlabs.count.ctab.CTab > CTabRunLog.txt 2>&1 
