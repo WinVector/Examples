6b3d0cb826e03759e0da29f64a9a3dbd32342bf7
John Mount <jmount@win-vector.com>
2014-10-06 18:16:15 -0700

    back to triple lock strategy

--- Count/src/com/mzlabs/count/op/SolnCache.java
+++ Count/src/com/mzlabs/count/op/SolnCache.java
@@ -3,38 +3,24 @@ package com.mzlabs.count.op;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.Map;
 import java.util.TreeMap;
 
 import com.mzlabs.count.util.IntVec;
 
 public final class SolnCache {
+	private final int storeMask = 0x07fff; // 2^k-1
 	
-	private static final class StoreEntry {
-		public final int[] key;
-		public final BigInteger value;
-		public long lastAccess;
-		
-		public StoreEntry(final int[] key, final BigInteger value, final long lastAccess) {
-			this.key = key;
-			this.value = value;
-			this.lastAccess = lastAccess;
-		}
+	private static final class SolnEntry {
+		public BigInteger value = null; 
 	}
 	
-	private final int nStores = 1377;
-	private final long bigSize = 5000000;
-	private final int initialSizeTargetI = (int)(bigSize/nStores) + 5;
-	private final long baseTimeMS;
-	private final ArrayList<Map<int[],StoreEntry>> hotStores = new ArrayList<Map<int[],StoreEntry>>(nStores);  // sub-stores (to shallow trees), also sync on these
-	private final int[] storeBound = new int[nStores]; // heuristic triggers for expensive attempted cache shrink
+	private final ArrayList<Map<int[],SolnEntry>> hotStores = new ArrayList<Map<int[],SolnEntry>>(storeMask+1);  // sub-stores (to shallow trees), also sync on these
 	
-	
+
 	public SolnCache() {
-		baseTimeMS = System.currentTimeMillis();
-		for(int i=0;i<nStores;++i) {
-			hotStores.add(new TreeMap<int[],StoreEntry>(IntVec.IntComp));
+		for(int i=0;i<=storeMask;++i) {
+			hotStores.add(new TreeMap<int[],SolnEntry>(IntVec.IntComp));
 		}
 		clear(); // get into initial state
 	}
@@ -46,96 +32,32 @@ public final class SolnCache {
 	 * @return
 	 */
 	public BigInteger evalCached(final CachableCalculation f, final int[] xin) {
-		// find the sub-store
-		final long time = System.currentTimeMillis();
-		final int storeIndex;
-		final Map<int[],StoreEntry> hotStore;
-		{
-			int subi = Arrays.hashCode(xin)%nStores;
-			if(subi<0) {
-				subi += nStores;
+		final SolnEntry foundValueHolder;
+		final SolnEntry newValueHolder = new SolnEntry();
+		synchronized (newValueHolder) {
+			// find the sub-store
+			final int storeIndex = Arrays.hashCode(xin)&storeMask;
+			final Map<int[],SolnEntry> hotStore = hotStores.get(storeIndex);
+			// hope for cached
+			synchronized (hotStore) {
+				foundValueHolder = hotStore.get(xin);
+				if(null==foundValueHolder) {
+					final int[] xcopy = Arrays.copyOf(xin,xin.length);
+					hotStore.put(xcopy,newValueHolder); // newValueHolder now visible to other threads, don't release it's lock before calculating
+				}
 			}
-			storeIndex = subi;
-			hotStore = hotStores.get(storeIndex);
-		}
-		// hope for cached
-		synchronized (hotStore) {
-			final StoreEntry found = hotStore.get(xin);
-			if(null!=found) {
-				found.lastAccess = time;
-				return found.value;
+			if(null==foundValueHolder) {
+				// do the work (while not holding outer locks, but while holding newValueHolder lock)
+				newValueHolder.value = f.eval(xin);
+				return newValueHolder.value;
 			}
 		}
-		// do the work (while not holding locks)
-		final int[] xcopy = Arrays.copyOf(xin,xin.length);
-		final BigInteger value = f.eval(xcopy);
-		final StoreEntry entry = new StoreEntry(xcopy,value,time);
-		// write back result
-		boolean considerReorg = false;
-		synchronized (hotStore) {
-			hotStore.put(xcopy,entry);
-			considerReorg = hotStore.size()>=storeBound[storeIndex];
+		// foundValueHolder not null here
+		synchronized (foundValueHolder) {
+			return foundValueHolder.value;
 		}
-		if(considerReorg) {
-			possiblyReorg();
-		}
-		return value;
 	}
 	
-	/**
-	 * assumes no locks held when this is called
-	 * expensive chech and re-org.  amortize this to cheap by using double per-sub store count guards.
-	 * trying to be a amortized cheap approximate LRU
-	 */
-	private void possiblyReorg() {
-		synchronized (this) { // there may be other operations working in parallel to this reorg, but no other reorg
-			// estimate sizes and reset individual triggers
-			long szEst = 0;
-			for(int i=0;i<nStores;++i) {
-				final Map<int[],StoreEntry> s = hotStores.get(i);
-				synchronized (s) {
-					final int szi = s.size();
-					szEst += szi;
-					storeBound[i] = 2*szi + initialSizeTargetI; // move trigger up to prevent a soon re-trigger
-				}
-			}
-			if(szEst<=bigSize) {
-				return;  // total size still in bounds, take no more action
-			}
-			// total size too large, shrink the stores, reset the triggers
-			// use mean access time as a approximation for median access time
-			long sumTimes = 0;
-			long totEntries = 0;
-			for(int i=0;i<nStores;++i) {
-				final Map<int[],StoreEntry> s = hotStores.get(i);
-				synchronized (s) {
-					for(final StoreEntry v: s.values()) {
-						sumTimes += v.lastAccess-baseTimeMS;
-						totEntries += 1;
-					}
-				}
-			}
-			final long meanTime = (long)Math.ceil(sumTimes/(double)totEntries);
-			// prune stores and reset triggers
-			long newSize = 0;
-			for(int i=0;i<nStores;++i) {
-				final Map<int[],StoreEntry> s = hotStores.get(i);
-				synchronized (s) {
-					final StoreEntry[] vals = s.values().toArray(new StoreEntry[s.size()]);
-					s.clear();
-					for(final StoreEntry v: vals) {
-						if(v.lastAccess-baseTimeMS>meanTime) {
-							s.put(v.key,v);
-						}
-					}
-					final int szi = s.size();
-					newSize += szi;
-					storeBound[i] = 2*szi + initialSizeTargetI; // move trigger up to prevent a soon re-trigger
-				}
-			}
-			System.err.println("#\tinfo: reorg stores " + szEst + " -> " + newSize + "\t" + new Date());
-		}
-	}
 
 	/**
 	 * best effort, not atomic across stores; but still fairly expensive because of lock acquisition costs
@@ -143,8 +65,8 @@ public final class SolnCache {
 	 */
 	public long size() {
 		long size = 0;
-		for(int i=0;i<nStores;++i) {
-			final Map<int[],StoreEntry> s = hotStores.get(i);
+		for(int i=0;i<=storeMask;++i) {
+			final Map<int[],SolnEntry> s = hotStores.get(i);
 			synchronized (s) {
 				size += s.size();
 			}
@@ -156,11 +78,10 @@ public final class SolnCache {
 	 * best effort, not atomic across stores
 	 */
 	public void clear() {
-		for(int i=0;i<nStores;++i) {
-			final Map<int[],StoreEntry> s = hotStores.get(i);
+		for(int i=0;i<=storeMask;++i) {
+			final Map<int[],SolnEntry> s = hotStores.get(i);
 			synchronized (s) {
 				s.clear();
-				storeBound[i] = initialSizeTargetI;
 			}
 		}
 	}
