4f417488fb629f3227f59d57dcfbf6af99920b1d
John Mount <jmount@win-vector.com>
2023-09-07 16:33:32 -0700

    move to dictionary types and recursive implementations

--- arg_types/type_signature.py
+++ arg_types/type_signature.py
@@ -1,87 +1,114 @@
 
 
 from functools import wraps
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Set, Type, Union
 from inspect import signature
 import pprint
 import numpy as np
 import pandas as pd
 
-def _prep_type_arg(v):
-    """
-    Transform/enforce type, set of types, or pandas data frame with types or sets of types columns.
-    """
-    assert v is not None
-    if isinstance(v, type):
-        return {v}
-    elif isinstance(v, set):
-        assert len(v) > 0
-        for vi in v:
-            assert(isinstance(vi, type))
-    elif isinstance(v, pd.DataFrame):
-        v = v.reset_index(drop=True, inplace=False)
-        assert v.shape[1] >= 0
-        assert v.shape[0] < 2
-        if v.shape[0] > 0:
-            for col_name in list(v.columns):
-                vj = v[col_name].values[0]
-                if vj is not None:
-                    if isinstance(vj, type):
-                        vj = {vj}
-                        v[col_name].values[0] = vj
-                    assert isinstance(vj, set)
-                    assert len(vj) > 0
-                    for vij in vj:
-                        assert(isinstance(vij, type))
-        return v
-    return {type(v)}
 
-class TypeSignature:
+def _prep_type_arg(v) -> Optional[Union[Type, Set, Dict]]:
+    """
+    Transform/enforce type, set of types, or dict of name to type maps to standard form.
+    None represents no constraint.
+    Types and sets of Types represent type constraints.
+    Dicts represent column structure of a data frame.
+    """
+    if v is None:
+        return None
+    elif isinstance(v, type):
+        return v
+    elif isinstance(v, set):
+        new_set = {_prep_type_arg(vi) for vi in v}
+        new_set = {vi for vi in v if v is not None}
+        for vi in new_set:
+            assert not isinstance(vi, set)
+        return new_set
+    elif isinstance(v, dict):
+        new_set = {ki: _prep_type_arg(vi) for ki, vi in v.items()}
+        for vi in new_set:
+            assert not isinstance(vi, dict)
+        return new_set
+    else:
+        return type(v)
+
+
+def _type_name(t) -> str:
+    if not isinstance(t, type):
+        t = type(t)
+    return t.__name__
+
+
+def non_null_types_in_frame(d: pd.DataFrame) -> Dict[str, Optional[Set[Type]]]:
+    """
+    Return dictionary of non-null types seen in dataframe columns.
+    """
+    assert isinstance(d, pd.DataFrame)
+    result = dict()
+    for col_name in d.columns:
+        types_seen = {type(vi) for vi in d[col_name].values if not pd.isnull(vi)}
+        if len(types_seen) < 1:
+            result[col_name] = None
+        else:
+            result[col_name] = types_seen
+    return result
+
+
+class TypeSignatureRaises:
     """
     Class as a type checking decorator.
     """
     def __init__(self, arg_specs: Optional[Dict[str, Any]]=None, *, return_spec=None) -> None:
         """
-        Check argument types and "return_spec".
-        Types must be in specified set. Pandas data frames must have at least declared columns and expected types in those columns.
         Wraps function to raise TypeError on argument non-compliance.
+        Types must be in specified set. 
+        Pandas data frames must have at least declared columns and no unexpected types in columns.
+        Nulls/Nones/NaNs values are not considered to have type (treating them as missingness).
+        None as type constraints are considered no-type (unfailable).
 
         :param arg_specs: dictionary of named args to type specifications.
         :param return_spec: optional return type specification.
         """
-        if arg_specs is None:
-            arg_specs = dict()
-        self.arg_specs = {k: _prep_type_arg(v) for k, v in arg_specs.items()}
-        self.return_spec = None
-        if return_spec is not None:
-            self.return_spec = _prep_type_arg(return_spec)
+        self.arg_specs = _prep_type_arg(arg_specs)
+        self.return_spec = _prep_type_arg(return_spec)
     
-    def _check_spec(self, *, expected_type, observed_value) -> Optional[str]:
+    def _check_spec(self, *, expected_type: Optional[Union[Type, Set, Dict]], observed_value) -> Optional[str]:
         if expected_type is None:
+            # no expectation, no failure possible
             return None
-        if isinstance(expected_type, set):
+        elif isinstance(expected_type, type):
+            # single type
+            if not isinstance(observed_value, expected_type):
+                observed_type = type(observed_value)
+                return f"expected type {_type_name(expected_type)}, found type {_type_name(observed_type)}"
+        elif isinstance(expected_type, set):
+            # type set, expect value to be one of the types
             if not np.any([isinstance(observed_value, ti) for ti in expected_type]):
                 observed_type = type(observed_value)
-                tnames = "{" + ", ".join(sorted([t.__name__ for t in expected_type])) + "}"
-                return f"one of {tnames} expected, was type {observed_type.__name__}"
-        elif isinstance(expected_type, pd.DataFrame):
+                type_names = "{" + ", ".join(sorted([_type_name(t) for t in expected_type])) + "}"
+                return f"expected type one of {type_names}, found type {_type_name(observed_type)}"
+        elif isinstance(expected_type, dict):
             if not isinstance(observed_value, pd.DataFrame):
                 observed_type = type(observed_value)
-                return f"should be type pandas.DataFrame, was type {observed_type.__name__}"
+                return f"expected type pandas.DataFrame, found type {_type_name(observed_type)}"
             msgs = []
-            for col_name in expected_type.columns:
+            for col_name, spec_i in expected_type.items():
                 if col_name not in observed_value.columns:
                     msgs.append(f"missing required column '{col_name}'")
                 else:
-                    if (expected_type.shape[0] > 0) and (observed_value.shape[0] > 0):
-                        expected_type_set = expected_type[col_name].values[0]
-                        if expected_type_set is not None:
-                            for vi in observed_value[col_name]:
-                                if (pd.isnull(vi) == False) and (not np.any([isinstance(vi, ti) for ti in expected_type_set])):
-                                    tnames = "{" + ", ".join(sorted([t.__name__ for t in expected_type_set])) + "}"
-                                    msgs.append(f" column '{col_name}' has a type {type(vi).__name__} entry, when one of {tnames} expected")
+                    if (spec_i is not None) and (observed_value.shape[0] > 0):
+                        for vi in observed_value[col_name]:
+                            if not pd.isnull(vi):
+                                msg_i = self._check_spec(expected_type=spec_i, observed_value=vi)
+                                if msg_i is not None:
+                                    msgs.append(f" column '{col_name}' {msg_i}")
+                                    break
             if len(msgs) > 0:
                 return ", ".join(msgs)
+        else:
+            # should not be reached
+            raise ValueError(f"expected type of type {type(expected_type)} unexpected")
         return None
 
     def check_args(self, *, arg_names: List[str], fname: str, args, kwargs) -> None:
@@ -97,25 +124,25 @@ class TypeSignature:
                 expected_type = self.arg_specs[k]
                 msg = self._check_spec(expected_type=expected_type, observed_value=observed_value)
                 if msg is not None:
-                    msgs.append(f"{fname}() arg {k} {msg}")
+                    msgs.append(f"arg {k} {msg}")
         # check types of named arguments
         for k, expected_type in self.arg_specs.items():
             if k not in seen:
                 if k not in kwargs.keys():
-                    msgs.append(f"{fname}() expected arg {k}")
+                    msgs.append(f"expected arg {k} missing")
                 else:
                     observed_value = kwargs[k]
                     msg = self._check_spec(expected_type=expected_type, observed_value=observed_value)
                     if msg is not None:
-                        msgs.append(f"{fname}() arg {k} {msg}")
+                        msgs.append(f"arg {k} {msg}")
         if len(msgs) > 0:
-            raise TypeError("\n".join(msgs))
+            raise TypeError("\nfunction " + fname + "(), issues:\n" + "  \n".join(msgs))
 
     def check_return(self, *, fname: str, return_value) -> None:
         assert isinstance(fname, str)
         msg = self._check_spec(expected_type=self.return_spec, observed_value=return_value)
         if msg is not None:
-            raise TypeError(f"{fname}() return value: {msg}")
+            raise TypeError(f"{fname}() return value: {msg}", return_value)
 
     def __call__(self, type_check_fn):
         """
@@ -126,9 +153,15 @@ class TypeSignature:
         type_check_arg_names = [k for k, v in signature(type_check_fn).parameters.items()]
         @wraps(type_check_fn)
         def wrapped_fn(*args, **kwargs):
-            type_check_self.check_args(fname=type_check_fn_name, arg_names=type_check_arg_names, args=args, kwargs=kwargs)
+            type_check_self.check_args(
+                fname=type_check_fn_name, 
+                arg_names=type_check_arg_names, 
+                args=args, 
+                kwargs=kwargs)
             type_check_return_value = type_check_fn(*args, **kwargs)
-            type_check_self.check_return(fname=type_check_fn_name, return_value=type_check_return_value)
+            type_check_self.check_return(
+                fname=type_check_fn_name, 
+                return_value=type_check_return_value)
             return type_check_return_value
         type_doc = (
             "\n arg specifications\n" + pprint.pformat(self.arg_specs)
@@ -139,3 +172,12 @@ class TypeSignature:
         else:
             wrapped_fn.__doc__ = type_doc + "\n\n" + type_check_fn.__doc__
         return wrapped_fn
+
+
+class TypeSignatureNOOP:
+    def __init__(self, arg_specs: Optional[Dict[str, Any]]=None, *, return_spec=None) -> None:
+        """Does nothing."""
+        pass
+
+    def __call__(self, type_check_fn):
+        return type_check_fn
