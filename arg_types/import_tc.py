4861673c1b1114b670ff506d9b4f7661ce6a1b0b
John Mount <jmount@win-vector.com>
2023-09-07 17:30:32 -0700

    demonstrate global re-bind and global switch

---
+++ arg_types/import_tc.py
@@ -0,0 +1,6 @@
+
+
+from type_signature import TypeCheckSwitch
+# from type_signature import TypeSignatureNOOP as TypeSignature
+from type_signature import TypeSignatureRaises as TypeSignature
+TypeCheckSwitch().on()
