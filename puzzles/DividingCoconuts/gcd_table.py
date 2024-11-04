771bfd2c8e500754ef4072227024f96b4cc23822
John Mount <jmount@win-vector.com>
2024-11-04 09:48:43 -0800

    pass back intermediate tables in a better way

--- puzzles/DividingCoconuts/gcd_table.py
+++ puzzles/DividingCoconuts/gcd_table.py
@@ -1,18 +1,16 @@
-from typing import List, Optional
+from typing import Any, List, Optional
 import numpy as np
 import pandas as pd
 from IPython.display import display
-
-
-captured_tables = []
 
 
 def _display_intermediate_forward_table(
         result: List[pd.DataFrame],
         *, 
         do_display: bool = True,
-        row_count_hint: Optional[int] = None) -> None:
-    global captured_tables
+        row_count_hint: Optional[int] = None,
+        captured_tables: Optional[List[Any]] = None,
+        ) -> None:
     if not do_display:
         return
     result = pd.concat(result, ignore_index=True)
@@ -28,17 +26,22 @@ def _display_intermediate_forward_table(
             return ['background-color: lightgreen' if col_name in ['b', 'a%b'] else '' for col_name in x.index]
         return ['' for _ in x]
     if row_id <= 0:
-        result.attrs['note'] = f"build row {row_id}: start (a > b)"
+        result.attrs['note'] = f"build row {row_id}: start (a >= b)"
     else:
         result.attrs['note'] = f"build row {row_id}: a[{row_id}]=b[{row_id-1}], b[{row_id}]=(a%b)[{row_id-1}]"
     styled_table = result.style.apply(highlight_rowval, axis=1).format(na_rep='')
-    captured_tables.append(styled_table)
+    if captured_tables is not None:
+        captured_tables.append(styled_table)
     display(styled_table.data.attrs['note'])
     display(styled_table)
 
 
-def _display_final_forward_table(result: pd.DataFrame, *, do_display: bool = True) -> None:
-    global captured_tables
+def _display_final_forward_table(
+        result: pd.DataFrame, 
+        *, 
+        do_display: bool = True,
+        captured_tables: Optional[List[Any]] = None,
+        ) -> None:
     if not do_display:
         return
     result = result.reset_index(drop=True, inplace=False)  # copy to prevent interference
@@ -55,13 +58,18 @@ def _display_final_forward_table(result: pd.DataFrame, *, do_display: bool = Tru
         return res
     result.attrs['note'] = f"finish with row {result.shape[0] - 1}: a[{row_id}]=b[{row_id-1}], b[{row_id}]=(a%b)[{row_id-1}], GCD= a[{row_id}]"
     styled_table = result.style.apply(highlight_rowcol, axis=1).format(na_rep='')
-    captured_tables.append(styled_table)
+    if captured_tables is not None:
+        captured_tables.append(styled_table)
     display(styled_table.data.attrs['note'])
     display(styled_table)
 
 
-def _display_backfill_step(result: pd.DataFrame, *, i:int, do_display: bool = True) -> None:
-    global captured_tables
+def _display_backfill_step(
+        result: pd.DataFrame, 
+        *, i:int, 
+        do_display: bool = True,
+        captured_tables: Optional[List[Any]] = None,
+        ) -> None:
     if not do_display:
         return
     result = result.reset_index(drop=True, inplace=False)  # copy to prevent interference
@@ -83,7 +91,8 @@ def _display_backfill_step(result: pd.DataFrame, *, i:int, do_display: bool = Tr
                     res[col_i] = 'background-color: lightgreen'
         return res
     styled_table = result.style.apply(highlight_rowcol, axis=1).format(na_rep='')
-    captured_tables.append(styled_table)
+    if captured_tables is not None:
+        captured_tables.append(styled_table)
     display(styled_table.data.attrs['note'])
     display(styled_table)
 
@@ -97,7 +106,6 @@ def build_gcd_table(a: int, b: int,
     :return: extended GCD work table (not backfilled, see back_fill_gcd_table())
     Note: may swap a, b to establish entry invariant.
     """
-    global captured_tables
     a, b = int(np.abs(a)), int(np.abs(b))
     if b > a:
         a, b = b, a
@@ -105,21 +113,23 @@ def build_gcd_table(a: int, b: int,
     row_count_hint = None
     if verbose:
         row_count_hint = build_gcd_table(a, b, verbose=False).shape[0]
-        captured_tables.clear()
+    captured_tables = []
     result = []
     while (b > 0) and (a > b):
         q = a // b  # quotient
         r = a - b * q  # remainder
         row = pd.DataFrame({"a": [a], "b": [b], "a%b": [r], "a//b": [q], "GCD(a, b)": None})
         result.append(row)
-        _display_intermediate_forward_table(result, do_display=verbose, row_count_hint=row_count_hint)
+        _display_intermediate_forward_table(
+            result, do_display=verbose, row_count_hint=row_count_hint, captured_tables=captured_tables)
         # prepare for next step using gcd(a, b) = gcd(b, r)
         a, b = b, r
     row = pd.DataFrame({"a": [a], "b": [b], "a%b": ["N/A"], "a//b": ["N/A"], "GCD(a, b)": None})
     result.append(row)
     gcd_table = pd.concat(result, ignore_index=True)
     gcd_table["GCD(a, b)"] = a
-    _display_final_forward_table(gcd_table, do_display=verbose)
+    _display_final_forward_table(gcd_table, do_display=verbose, captured_tables=captured_tables)
+    gcd_table.attrs['captured_tables'] = captured_tables
     return gcd_table
 
 
@@ -132,12 +142,17 @@ def back_fill_gcd_table(result: pd.DataFrame, *, verbose: bool = False) -> None:
     result.loc[result.shape[0] - 1, "u"] = 1
     result["v"] = None
     result.loc[result.shape[0] - 1, "v"] = 0
-    _display_backfill_step(result, i=result.shape[0] - 1, do_display=verbose)
+    try:
+        captured_tables = result.attrs['captured_tables']
+    except KeyError:
+        captured_tables = None
+    _display_backfill_step(
+        result, i=result.shape[0] - 1, do_display=verbose, captured_tables=captured_tables)
     for i in reversed(range(result.shape[0] - 1)):
         result.loc[i, "u"] = result.loc[i + 1, "v"]
         result.loc[i, "v"] = (result.loc[i + 1, "u"] 
                               - result.loc[i, "a//b"] * result.loc[i + 1, "v"])
-        _display_backfill_step(result, i=i, do_display=verbose)
+        _display_backfill_step(result, i=i, do_display=verbose, captured_tables=captured_tables)
     assert np.all(result["u"] * result["a"] + result["v"] * result["b"] == result["GCD(a, b)"])
 
 
