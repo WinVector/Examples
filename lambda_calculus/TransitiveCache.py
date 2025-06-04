3b3c63f575db1837d93000bce4eb75dbeaf137cb
John Mount <jmount@win-vector.com>
2025-06-04 08:12:40 -0700

    demonstrate cache

--- lambda_calculus/TransitiveCache.py
+++ lambda_calculus/TransitiveCache.py
@@ -1,28 +1,43 @@
 
 from typing import Dict, Set
+from collections import OrderedDict
 
 
 class TransitiveCache:
     """cache that store start->end _transitions and compute transitive closure on lookup"""
-    _absorbing_states: Set
-    _transitions: Dict
+    _absorbing_states: OrderedDict
+    _transitions: OrderedDict
+    _max_size: int
 
-    def __init__(self):
-        self._absorbing_states = set()
-        self._transitions = dict()
+    def __init__(self, *, max_size : int = 1000000):
+        assert isinstance(max_size, int)
+        self._absorbing_states = OrderedDict()
+        self._transitions = OrderedDict()
+        self._max_size = max_size
 
     def store_transition(self, start, end) -> None:
         assert start is not None
         assert end is not None
         if start == end:
-            self._absorbing_states.add(start)
+            self._absorbing_states[start] = True
+            while len(self._absorbing_states) > self._max_size:
+                self._absorbing_states.popitem(last=False)
         else:
             self._transitions[start] = end
+            while len(self._transitions) > self._max_size:
+                self._transitions.popitem(last=False)
     
     def __contains__(self, item):
         assert item is not None
-        return (item in self._absorbing_states) or (item in self._transitions)
-    
+        have = False
+        if item in self._absorbing_states:
+            self._absorbing_states.move_to_end(item)
+            have = True
+        if (not have) and (item in self._transitions):
+            self._transitions.move_to_end(item)
+            have = True
+        return have
+
     def lookup_result(self, start):
         assert start is not None
         if start not in self:
@@ -39,4 +54,5 @@ class TransitiveCache:
             pass
         if start != end:
             self.store_transition(start, end)
+            self._transitions.move_to_end(start)
         return end
